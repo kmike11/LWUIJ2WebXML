ea7a565556f8bd11efe6e2e2d9c5462765de401d
kmike11 <k.michael1154@gmail.com>
2024-11-12 16:24:50 +0100

    kodfeltolt

---
+++ LWUIJ2_1112/JSONParseLWUIJ2/src/lwuij21112/JSONWriteLWUIJ2.java
@@ -0,0 +1,15 @@
+package lwuij21112;
+
+import java.io.FileWriter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONWriteLWUIJ2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
